4f15e0ebe636e8fdd1c88430986fac1e81bc3a56
ABDELALI AIT TALB <abait-ta@student.1337.ma>
2023-12-05 20:25:15 +0100

    Finish norm

--- Global_main.c
+++ Global_main.c
@@ -6,15 +6,11 @@
 /*   By: abait-ta <abait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:37:41 by abait-ta          #+#    #+#             */
-/*   Updated: 2023/12/05 20:07:29 by abait-ta         ###   ########.fr       */
+/*   Updated: 2023/12/05 20:24:46 by abait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./HEADER/Parsing.h"
-#include "./HEADER/game.h"
-
-// const char	*path_type[] = {"NORD", "SOUTH", "WEST", "EAST"};
-// const char	*color_type[] = {"FLOOR", "CIEL"};
 
 void	extensionvalidity(char *filename)
 {
@@ -26,27 +22,6 @@ void	extensionvalidity(char *filename)
 	if (check != 0)
 		extension_error();
 }
-
-// void	print_textures(t_textures **begin)
-// {
-// 	t_textures	*cursur;
-// 	if (!(*begin))
-//     {
-//         write(1, "EMPTY\n", 6);
-// 		return ;
-//     }
-//     cursur = (*begin);
-// 	printf("***********************************************************\n");
-// 	printf("*       PATH                    |           PATHTYPE      *\n");
-// 	printf("***********************************************************\n");
-// 	while (cursur)
-// 	{
-// 		printf("*%-20s\t|\t%-10s\t  *\n", cursur->path,
-// 			path_type[cursur->type]);
-// 		printf("***********************************************************\n");
-// 		cursur = cursur->next;
-// 	}
-// }
 
 void	parsing(char **av, t_gamedata *data)
 {
@@ -76,35 +51,6 @@ void	initial_check(int *fd, int ac, char **av)
 	if (*fd == -1)
 		display_error(strerror(errno));
 }
-
-// void    leaks()
-// {
-//     system("leaks cub");
-// }
-
-// void    print_color(t_rgb **begin)
-// {
-//     t_rgb   *cursur;
-//     int     i;
-//     i = 0;
-// 	if (!(*begin))
-//     {
-//         write(1, "EMPTY\n", 6);
-// 		return ;
-//     }
-//     cursur = (*begin);
-// 	printf("*********************************\n");
-// 	printf("*       colors     |   TYPE     *\n");
-// 	printf("*********************************\n");
-// 	while (cursur)
-// 	{
-//         printf("r: %lu,g: %lu, b: %lu | %s\t*\n", 
-//             cursur->rgb[0], cursur->rgb[1], cursur->rgb[2],
-	// color_type[cursur->type]);
-// 		printf("*********************************\n");
-// 		cursur = cursur->next;
-// 	}
-// }
 
 int	main(int ac, char **av)
 {
